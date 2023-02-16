07b1a3ad0fe0353ddc6ac6febde72e01e5719f3e
Abdishakur Abdalla Gesey <karbuuno@gmail.com>
2023-02-16 22:50:21 +0000

    adding comments in scriot.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // Assignment Code
+// creating an object
 var characters = {
   upperCase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
   lowerCase: "abcdefghijklmnopqrstuvwxyz",
@@ -16,7 +17,7 @@ var generatePassword = function (characters) {
 
     isTrue = length && Number(length) && length >= 8 && length <= 128;
   } while (!isTrue);
-
+  //Asking user if the need to include lowerCase
   var lowerCase = window.confirm("would you like to include lowercase letters");
 
   if (lowerCase) {
@@ -24,25 +25,28 @@ var generatePassword = function (characters) {
   } else {
     window.alert("lowerCase will not include in your password");
   }
+  //Asking user if the need to include upperCase
   var upperCase = window.confirm("would you like to include upperCase letters");
   if (upperCase) {
     newPassword += characters["upperCase"];
   } else {
     window.alert("UpperCase will not include in your password");
   }
+  //Asking user if the need to include Symbols
   var Symbols = window.confirm("would you like to include Symbols");
   if (Symbols) {
     newPassword += characters["Symbols"];
   } else {
     window.alert("Symbols will not include in your password");
   }
+  //Asking user if the need to include numbers
   var numbers = window.confirm("would you like to include numbers");
   if (numbers) {
     newPassword += characters["numbers"];
   } else {
     window.alert("Numbers will not include in your password");
   }
-
+  // if user not choose anything will ask to choose one of the following characters.
   if (!upperCase && !lowerCase && !numbers && !Symbols) {
     window.alert(
       "Please choose one of the following strings upperCase, lowerCase, numbers or Symbols "
@@ -50,11 +54,9 @@ var generatePassword = function (characters) {
   } else {
     for (var i = 0; i < length; i++) {
       let currentRandom = Math.floor(Math.random() * newPassword.length);
-
-      // selecting on of the sybmoles or number or letters
       finalPassword += newPassword[currentRandom];
     }
-    alert("Your passwor has been secsefully generated");
+    window.alert("Your passwor has been secsefully generated");
   }
 
   return finalPassword;
