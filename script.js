2ca0ee28b0669869969809f0a9e63171ec2bc740
Abdishakur Abdalla Gesey <karbuuno@gmail.com>
2023-02-16 13:29:25 +0000

    added wil do while

--- script.js
+++ script.js
@@ -9,54 +9,52 @@ var characters = {
 var generatePassword = function (characters) {
   var newPassword = "";
   var finalPassword = "";
-  var length = window.prompt(
-    "Ente between 8 -128 of characters you would like your password to be"
-  );
-  if (length && Number && length > 8 && length < 128) {
-    var lowerCase = window.confirm(
-      "would you like to include lowercase letters"
-    );
+  var length;
+  let isTrue;
+  do {
+    length = window.prompt("Please Enter A Number between 8-128");
 
-    if (lowerCase) {
-      newPassword += characters["lowerCase"];
-    } else {
-      window.alert("lowerCase will not include in your password");
-    }
-    var upperCase = window.confirm(
-      "would you like to include upperCase letters"
-    );
-    if (upperCase) {
-      newPassword += characters["upperCase"];
-    } else {
-      window.alert("UpperCase will not include in your password");
-    }
-    var Symbols = window.confirm("would you like to include Symbols");
-    if (Symbols) {
-      newPassword += characters["Symbols"];
-    } else {
-      window.alert("Symbols will not include in your password");
-    }
-    var numbers = window.confirm("would you like to include numbers");
-    if (numbers) {
-      newPassword += characters["numbers"];
-    } else {
-      window.alert("Numbers will not include in your password");
-    }
+    isTrue = length && Number(length) && length >= 8 && length <= 128;
+  } while (!isTrue);
 
-    if (!upperCase && !lowerCase && !numbers && !Symbols) {
-      window.alert(
-        "Please choose one of the following strings upperCase, lowerCase, numbers or Symbols "
-      );
-    } else {
-      for (var i = 0; i < length; i++) {
-        finalPassword +=
-          newPassword[Math.floor(Math.random() * newPassword.length)];
-      }
-      alert("Your passwor has been secsefully generated");
-    }
+  var lowerCase = window.confirm("would you like to include lowercase letters");
+
+  if (lowerCase) {
+    newPassword += characters["lowerCase"];
   } else {
-    window.alert("Pleace enter a numebr between 8 and 128");
-    return "Pleace try again";
+    window.alert("lowerCase will not include in your password");
+  }
+  var upperCase = window.confirm("would you like to include upperCase letters");
+  if (upperCase) {
+    newPassword += characters["upperCase"];
+  } else {
+    window.alert("UpperCase will not include in your password");
+  }
+  var Symbols = window.confirm("would you like to include Symbols");
+  if (Symbols) {
+    newPassword += characters["Symbols"];
+  } else {
+    window.alert("Symbols will not include in your password");
+  }
+  var numbers = window.confirm("would you like to include numbers");
+  if (numbers) {
+    newPassword += characters["numbers"];
+  } else {
+    window.alert("Numbers will not include in your password");
+  }
+
+  if (!upperCase && !lowerCase && !numbers && !Symbols) {
+    window.alert(
+      "Please choose one of the following strings upperCase, lowerCase, numbers or Symbols "
+    );
+  } else {
+    for (var i = 0; i < length; i++) {
+      let currentRandom = Math.floor(Math.random() * newPassword.length);
+
+      // selecting on of the sybmoles or number or letters
+      finalPassword += newPassword[currentRandom];
+    }
+    alert("Your passwor has been secsefully generated");
   }
 
   return finalPassword;
